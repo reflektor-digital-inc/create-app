9b446ad61a5da6233b05bfce957baca48d402093
wlto <william@reflektor.digital>
2022-01-13 09:07:34 -0500

    chore: remove github oauth token requirement, assume boilerplates are public

--- data/questions.js
+++ data/questions.js
@@ -9,10 +9,10 @@ const questions = {
     name: 'template',
     message: 'Which template would you like to use?',
     choices: [
-      { title: 'React + NextJS', value: 'react-next' },
       { title: 'React + Vite', value: 'react-vite' },
-      { title: 'Vue + NuxtJS 3', value: 'vue-nuxt' },
-      { title: 'Vue + Vite', value: 'vue-vite' }
+      { title: 'React + NextJS', value: 'react-next', disabled : true },
+      { title: 'Vue + Vite', value: 'vue-vite', disabled : true },
+      { title: 'Vue + NuxtJS 3', value: 'vue-nuxt', disabled : true }
     ],
     initial : 0
   }
